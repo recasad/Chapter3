d3afab5e731e2e6a7e5e530a40c670bf4a89ffd3
recasad <33033835+recasad@users.noreply.github.com>
2017-11-06 11:34:26 -0600

    Updated Variable Demo

---
+++ String.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub btnShowDate_Click(sender As Object, e As EventArgs) Handles btnShowDate.Click
+        lblDateString.Text = txtDayofWeek.Text & ", " &
+            txtMonth.Text & " " & txtDayofMonth.Text & ", " &
+            txtYear.Text
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDayofMonth.Clear()
+        txtDayofWeek.Clear()
+        txtMonth.Clear()
+        txtYear.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
