5b5bee5e0032ace12b99e7a592a7a4a0ea38bead
recasad <rc308750@students.davenport.k12.ia.us>
2017-11-13 11:35:23 -0600

    Update and complete Test Score Average

---
+++ Test Score Average/Test Score Average/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalculateAverage.Click
+        ' Declare variables for the calculations.
+        Dim dblAddedScores As Double
+        Dim dblAverage As Double
+        Const dblScores As Double = 5D
+
+        'Calculate the added scores.
+        dblAddedScores = CDbl(txtTestBox1.Text) + CDbl(txtTestBox2.Text) + CDbl(txtTestBox3.Text) + CDbl(txtTestBox4.Text) + CDbl(txtTestBox5.Text)
+
+        'Average the Scores and display average
+        dblAverage = dblAddedScores / dblScores
+        lblAverage.Text = dblAverage.ToString
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear TextBox Scores and Average Label
+        txtTestBox1.Clear()
+        txtTestBox2.Clear()
+        txtTestBox3.Clear()
+        txtTestBox4.Clear()
+        txtTestBox5.Clear()
+        lblAverage.Text = String.Empty
+
+        ' Set focus to txtTestBox1
+        txtTestBox1.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the form.
+        Me.Close()
+    End Sub
+End Class
