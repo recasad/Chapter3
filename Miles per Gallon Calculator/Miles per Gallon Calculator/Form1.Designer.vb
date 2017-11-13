ce90be52d5bfa11801c918cb6a92b8dd91594a5a
recasad <rc308750@students.davenport.k12.ia.us>
2017-11-13 09:17:21 -0600

    Update and Complete Miles per Gallon Calculator

---
+++ Miles per Gallon Calculator/Miles per Gallon Calculator/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.btnCalculateMPG = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblMPG = New System.Windows.Forms.Label()
+        Me.txtGallons = New System.Windows.Forms.TextBox()
+        Me.txtMiles = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 259)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(344, 22)
+        Me.StatusStrip1.TabIndex = 0
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'btnCalculateMPG
+        '
+        Me.btnCalculateMPG.Location = New System.Drawing.Point(49, 207)
+        Me.btnCalculateMPG.Name = "btnCalculateMPG"
+        Me.btnCalculateMPG.Size = New System.Drawing.Size(75, 39)
+        Me.btnCalculateMPG.TabIndex = 1
+        Me.btnCalculateMPG.Text = "Calculate MPG"
+        Me.btnCalculateMPG.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(130, 207)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 39)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(211, 207)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 39)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(46, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 49)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Gallons of gas the car can hold:"
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(46, 95)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 47)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Number of miles it can be driven on a full tank:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(46, 167)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Miles per gallon:"
+        '
+        'lblMPG
+        '
+        Me.lblMPG.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMPG.Location = New System.Drawing.Point(147, 167)
+        Me.lblMPG.Name = "lblMPG"
+        Me.lblMPG.Size = New System.Drawing.Size(156, 22)
+        Me.lblMPG.TabIndex = 7
+        '
+        'txtGallons
+        '
+        Me.txtGallons.Location = New System.Drawing.Point(147, 53)
+        Me.txtGallons.Name = "txtGallons"
+        Me.txtGallons.Size = New System.Drawing.Size(153, 20)
+        Me.txtGallons.TabIndex = 8
+        '
+        'txtMiles
+        '
+        Me.txtMiles.Location = New System.Drawing.Point(147, 109)
+        Me.txtMiles.Name = "txtMiles"
+        Me.txtMiles.Size = New System.Drawing.Size(153, 20)
+        Me.txtMiles.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(344, 281)
+        Me.Controls.Add(Me.txtMiles)
+        Me.Controls.Add(Me.txtGallons)
+        Me.Controls.Add(Me.lblMPG)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculateMPG)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Name = "Form1"
+        Me.Text = "Miles per Gallon Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents btnCalculateMPG As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblMPG As Label
+    Friend WithEvents txtGallons As TextBox
+    Friend WithEvents txtMiles As TextBox
+End Class
---
+++ Miles per Gallon Calculator/Miles per Gallon Calculator/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub btnCalculateMPG_Click(sender As Object, e As EventArgs) Handles btnCalculateMPG.Click
+        ' This event handler calculates Miles per Gallon.
+        ' Declare a variable to hold the result.
+        Dim dblResult As Double
+
+        ' Divide the two numbers and store the results in dblResult.
+        dblResult = CDbl(txtMiles.Text) / CDbl(txtGallons.Text)
+
+        ' Display the result.
+        lblMPG.Text = CStr(dblResult)
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear TextBox Miles and Gallons and Label Miles per Gallon.
+        txtGallons.Clear()
+        txtMiles.Clear()
+        lblMPG.Text = String.Empty
+
+        ' Set focus to txtGallons.
+        txtGallons.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the form.
+        Me.Close()
+    End Sub
+End Class
